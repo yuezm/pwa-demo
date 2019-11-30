e8eae0329ea4f8fec5495b16719a9261c442ff14
keven <yuezmkeven@gmail.com>
2019-11-30 23:19:15 +0800

    feat: 注册功能

---
+++ sw.js
@@ -0,0 +1,27 @@
+const self = this;
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      console.log('缓存response', response);
+      if (response) return response;
+
+      const request = event.request.clone();
+      return fetch(request).then(httpRes => {
+        const response = httpRes.clone();
+
+        if (httpRes.status === 200) {
+          caches.open('pwa-demo').then(cache => {
+            cache.put(event.request, response).then(() => {
+              console.log('添加缓存成功');
+            }).catch(() => {
+              console.log('添加缓存失败')
+            });
+          })
+        }
+
+        return httpRes;
+      });
+    })
+  )
+});
